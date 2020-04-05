f17c3ae1614b11645e9090f2ee9a20307d2b885f
huli <aszx87410@gmail.com>
2020-04-05 11:43:24 +0800

    Change style to atomDark

--- src/components/common/markdown.js
+++ src/components/common/markdown.js
@@ -1,11 +1,11 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vs } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import ReactMarkdown from "react-markdown";
 import React from "react";
 
 const CodeBlock = ({ language, value }) => {
   return (
-    <SyntaxHighlighter language={language} style={vs} showLineNumbers={true}>
+    <SyntaxHighlighter language={language} style={atomDark} showLineNumbers={true}>
       {value}
     </SyntaxHighlighter>
   );
