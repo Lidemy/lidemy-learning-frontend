3fae26415f1e9402d2f8d1a9aaffcbd327e5962a
minw <ishin4554@gmail.com>
2020-04-13 23:54:56 +0800

    [fix] 調整 inline-style 為 functional css

--- src/components/header/nameModal.js
+++ src/components/header/nameModal.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { Modal, Input, Tag } from "antd";
 
-
 const NameModal = ({ visible, instance, onCancel, onConfirm }) => {
-  const init = { 
-    nickname: "", 
-    slackId: "" 
+  const init = {
+    nickname: "",
+    slackId: ""
   };
-  const [ eachEntry, setEachEntry ] = useState(init);
+  const [eachEntry, setEachEntry] = useState(init);
   const { nickname, slackId, isTA, isAdmin, isStudent, role } = eachEntry;
 
   const handleConfirm = () => {
@@ -25,55 +24,48 @@ const NameModal = ({ visible, instance, onCancel, onConfirm }) => {
   };
 
   const handleInputChange = evt => {
-    setEachEntry({ 
-      ...eachEntry, 
-      [evt.target.name]: evt.target.value 
+    setEachEntry({
+      ...eachEntry,
+      [evt.target.name]: evt.target.value
     });
   };
 
-  useEffect(() => {   
-    setEachEntry({ 
-      ...instance
-    });
-  }, [instance]);
+  useEffect(
+    () => {
+      setEachEntry({
+        ...instance
+      });
+    },
+    [instance]
+  );
 
   return (
     <Modal
       title="個人資料"
       okText="送出"
       cancelText="取消"
-      onOk={ handleConfirm }
-      onCancel={ handleCancel }
-      visible={ visible }
+      onOk={handleConfirm}
+      onCancel={handleCancel}
+      visible={visible}
     >
-      <div style={{marginBottom: "10px"}}>
+      <div class="mb2">
         <label>身份：</label>
         <div>
           {isTA && <Tag color="green">助教</Tag>}
           {isAdmin && <Tag color="red">管理員</Tag>}
-          {isStudent && (
-            <Tag color="blue">第 {role} 期學生</Tag>
-          )}
+          {isStudent && <Tag color="blue">第 {role} 期學生</Tag>}
         </div>
       </div>
-      <div style={{marginBottom: "10px"}}>
+      <div class="mb2">
         <label>更改暱稱：</label>
-        <Input 
-          onChange={ handleInputChange } 
-          name="nickname" 
-          value={ nickname } 
-        />
+        <Input onChange={handleInputChange} name="nickname" value={nickname} />
       </div>
-      <div style={{marginBottom: "10px"}}>
+      <div class="mb2">
         <label>更改 Slack Id:</label>
-        <Input 
-          onChange={ handleInputChange } 
-          name="slackId" 
-          value={ slackId } 
-        />
+        <Input onChange={handleInputChange} name="slackId" value={slackId} />
       </div>
     </Modal>
   );
-}
- 
+};
+
 export default NameModal;
